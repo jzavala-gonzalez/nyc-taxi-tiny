d8570f44657e473d2c2dc958aa1b8a825534e686
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2022-11-19 23:27:07 -0400

    Example of pulling data using duckdb

---
+++ duckdb-import.py
@@ -0,0 +1,37 @@
+import duckdb
+
+con = duckdb.connect(':memory:')
+con.install_extension('httpfs')
+con.load_extension('httpfs')
+# con.execute('SELECT * FROM duckdb_extensions();')
+
+
+manifest_fname = 'fileurls.txt'
+with open(manifest_fname, 'r') as f:
+    urls = f.read().splitlines()
+print(urls)
+
+
+# Sample a few rows with all the columns of the dataset
+con.execute(f'''
+SELECT *
+FROM parquet_scan({urls[0:1]})
+LIMIT 5;
+''')
+sampledf = con.fetch_df()
+print(sampledf)
+print(sampledf.columns)
+print()
+
+
+# Count the number of taxi rides per year-month
+con.execute(f'''
+SELECT date_part('year', pickup_datetime) as pickup_year, date_part('month', pickup_datetime) as pickup_month, COUNT(*)
+FROM parquet_scan({urls[0:]})
+GROUP BY pickup_year, pickup_month
+ORDER BY pickup_year, pickup_month;
+''')
+df = con.fetch_df()
+print(df)
+print(df.columns)
+print()
--- taxi_download.py
+++ taxi_download.py
@@ -6,32 +6,45 @@ print('main.py')
 datadir = 'nyc-taxi-tiny'
 os.makedirs( datadir, exist_ok=True)
 
+manifest_fname = 'fileurls.txt'
+
 bucket = "https://voltrondata-labs-datasets.s3.us-east-2.amazonaws.com"
+pagesroot = 'https://jzavala-gonzalez.github.io/nyc-taxi-tiny/'
 
 years = list(range(2009,2023))
 months = list(range(1,13))
 
+urls = []
 for y in years:
-  for m in months:
-    dpath = os.path.join('nyc-taxi-tiny', f'year={y}', f'month={m}')
-    # print(dpath)
-    os.makedirs(dpath, exist_ok=True)
-    durl = os.path.join(bucket, dpath, "part-0.parquet")
-    #print(durl)
-    dfile = os.path.join(dpath, 'part-0.parquet')
-    if os.path.exists(dfile):
-      continue
+    for m in months:
+        dpath = os.path.join('nyc-taxi-tiny', f'year={y}', f'month={m}')
+        # print(dpath)
+        os.makedirs(dpath, exist_ok=True)
+        durl = os.path.join(bucket, dpath, "part-0.parquet")
+        purl = os.path.join(pagesroot, dpath, 'part-0.parquet')
+        #print(durl)
+        dfile = os.path.join(dpath, 'part-0.parquet')
+        if os.path.exists(dfile):
+            urls.append(purl)
+            continue
 
-    r = requests.get(durl)
-    if r.status_code != 200:
-      print(durl, r.status_code)
-      continue
+        r = requests.get(durl)
+        if r.status_code != 200:
+            print(durl, r.status_code)
+            continue
 
-    
-    with open(dfile, 'wb') as f:
-      f.write(r.content)
+        
+        with open(dfile, 'wb') as f:
+                f.write(r.content)
 
-  break
+        urls.append(purl)
+
+    break
+
+
+
+with open(manifest_fname, 'w') as f:
+    f.write('\n'.join(urls))
 
 
 # from dataset_check import *
